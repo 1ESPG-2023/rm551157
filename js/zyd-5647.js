27b7fcdb25628da22decf47d07e89f15802d10be
MatheusCosta616 <rm551157@fiap.com.br>
2023-04-11 11:46:18 -0300

    completo

--- js/zyd-5647.js
+++ js/zyd-5647.js
@@ -1,5 +1,14 @@
 const liElement = document.getElementsByTagName("li")
-
+/*
 for (let index = 0; index < liElement.length; index++) {
     console.log (liElement[index].textContent)
 }
+*/
+
+let nr1 = [1,2,3,4,5]
+let nr2 = [6,7,8,9,10]
+let nr3 =  [nr1,nr2]
+
+console.log(`arrays ai nr1 ${nr1}`)
+console.log(`arrays ai nr2 ${nr2}`)
+console.log(`arrays ai nr3 ${nr3}`)
