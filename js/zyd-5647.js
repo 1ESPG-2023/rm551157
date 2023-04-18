be31542ce9ad959de7eeede93bc863db0e21032d
MatheusCosta616 <rm551157@fiap.com.br>
2023-04-18 11:08:07 -0300

    Estrutura inicial para o ex func.

--- js/zyd-5647.js
+++ js/zyd-5647.js
@@ -1,14 +1,15 @@
-const liElement = document.getElementsByTagName("li")
 /*
+const liElement = document.getElementsByTagName("li")
+
 for (let index = 0; index < liElement.length; index++) {
     console.log (liElement[index].textContent)
 }
-*/
+
 
 let nr1 = [1,2,3,4,5]
 let nr2 = [6,7,8,9,10]
 let nr3 =  [nr1,nr2]
-
+/*
 console.log(`arrays ai nr1 ${nr1}`)
 console.log(`arrays ai nr2 ${nr2}`)
 nr3.forEach((itenDoArray => {
@@ -25,8 +26,11 @@ nr3.map((numero,key)=>{
 
 const liElementHtml = document.getElementsByClassName("li")
 const liElementArray = [...liElementHtml]
-liElementArray.map((item,key)=>{
-    console.log(`${key+1} -I ${item.textContent}`)
+liElementArray.map((item,key)=>{ 
+    if (item.textContent == "Item - 39"){
+        console.log(`${key+1} ${item.textContent} = "Localizado"`)
+    }
+    
 })
-
+*/
 
